e613ea8c7b48e1c4aa6df1f99250207836b781d2
Jimmi Christensen <jimm1403@eal.dk>
2017-09-22 15:37:01 +0200

    Finished clientHandler somewhat

--- AuctionHouseServer/ClientHandler.cs
+++ AuctionHouseServer/ClientHandler.cs
@@ -4,21 +4,43 @@ using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AuctionHouseServer
 {
 	class ClientHandler
 	{
+		Thread clientThread;
 		TcpClient client;
 		StreamReader SR;
 		StreamWriter SW;
+		NetworkStream nws;
+		public TcpClient Client { get { return client; } }
 
 		public ClientHandler(TcpClient newClient)
 		{
 			client = newClient;
-			SR = new StreamReader(client.GetStream());
-			SW = new StreamWriter(client.GetStream());
+
+			nws = client.GetStream();
+			SR = new StreamReader(nws);
+			SW = new StreamWriter(nws);
+		}
+
+		public void HandleClient()
+		{
+
+		}
+
+		public void StartClient()
+		{
+			clientThread = new Thread(HandleClient);
+			clientThread.Start();
+		}
+		public void SendToClient(string message)
+		{
+			SW.WriteLine(message);
+			SW.Flush();
 		}
 		 
 	}
