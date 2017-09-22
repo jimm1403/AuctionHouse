b51d15a7b02d8ecd934e32f589201e6879e29eb9
Jimmi Christensen <jimm1403@eal.dk>
2017-09-22 14:25:36 +0200

    Add clientHandler class, tcp, streamwriter, streamreader

---
+++ AuctionHouseServer/ClientHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionHouseServer
+{
+	class ClientHandler
+	{
+		TcpClient client;
+		StreamReader SR;
+		StreamWriter SW;
+
+		public ClientHandler(TcpClient newClient)
+		{
+			client = newClient;
+			SR = new StreamReader(client.GetStream());
+			SW = new StreamWriter(client.GetStream());
+		}
+		 
+	}
+}
